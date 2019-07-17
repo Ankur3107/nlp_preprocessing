ac9fd556b63fe72b211e44f522291684e0057f35
Ankur Kumar <ankur.kumar@adityabirla.com>
2019-07-17 11:28:47 +0530

    clean_function_added

--- text_preprocessing/clean.py
+++ text_preprocessing/clean.py
@@ -57,4 +57,86 @@ def _print_dict(temp_dict, n_items=10):
             break  
 
 
+#################### Main Function #################
+
+def to_lower(data):
+    if verbose: print('#'*10 ,'Step - Lowering everything:')
+    data = list(map(lambda x: x.lower(), data))
+    return data
+
+def to_normalize(data):
+    if verbose: print('#'*10 ,'Step - Normalize chars and dots:')
+
+    normalized_chars = {}
+    
+    chars = '‒–―‐—━—-▬'
+    for char in chars:
+        normalized_chars[ord(char)] = '-'
+
+    chars = '«»“”¨"'
+    for char in chars:
+        normalized_chars[ord(char)] = '"'
+
+    chars = "’'ʻˈ´`′‘’\x92"
+    for char in chars:
+        normalized_chars[ord(char)] = "'"
+
+    chars = '̲_'
+    for char in chars:
+        normalized_chars[ord(char)] = '_'
+
+    chars = '\xad\x7f'
+    for char in chars:
+        normalized_chars[ord(char)] = ''
+
+    chars = '\n\r\t\u200b\x96'
+    for char in chars:
+        normalized_chars[ord(char)] = ' '
+
+    # Normalize chars and dots - SEE HELPER FOR DETAILS
+    # Global
+    data = list(map(lambda x: ' '.join([_make_cleaning(i,normalized_chars) for i in x.split()]), data))
+    data = list(map(lambda x: re.sub('\(dot\)', '.', x), data))
+    data = list(map(lambda x: deaccent(x), data))
+    
+    return data
+
+def remove_control_char(data):
+    if verbose: print('#'*10 ,'Step - Control Chars:')
+    global_chars_list = list(set([c for line in data for c in line]))
+    chars_dict = {c:'' for c in global_chars_list if unicodedata.category(c)[0]=='C'}
+    data = list(map(lambda x: ' '.join([_make_cleaning(i,chars_dict) for i in x.split()]), data))
+    
+    return data
+
+def remove_href(data):
+    if verbose: print('#'*10 ,'Step - Remove hrefs:')
+    data = list(map(lambda x: re.sub(re.findall(r'\<a(.*?)\>', x)[0], '', x) if (len(re.findall(r'\<a (.*?)\>', x))>0) and ('href' in re.findall(r'\<a (.*?)\>', x)[0]) else x, data))
+    return data
+
+def remove_duplicate(data):
+    # Duplicated dots, question marks and exclamations
+    # Locallocal_vocab
+    if verbose: print('#' * 10, 'Step - Duplicated Chars:')
+    local_vocab={}
+    temp_vocab = _check_vocab(data, local_vocab, response='unknown_list')
+    temp_vocab = [k for k in temp_vocab if _check_replace(k)]
+    temp_dict = {}
+    
+    for word in temp_vocab:
+        new_word = word
+        if (Counter(word)['.']>1) or (Counter(word)['!']>1) or (Counter(word)['?']>1) or (Counter(word)[',']>1):
+            if (Counter(word)['.']>1):
+                new_word = re.sub('\.\.+', ' . . . ', new_word)
+            if (Counter(word)['!']>1):
+                new_word = re.sub('\!\!+', ' ! ! ! ', new_word)
+            if (Counter(word)['?']>1):
+                new_word = re.sub('\?\?+', ' ? ? ? ', new_word)
+            if (Counter(word)[',']>1):
+                new_word = re.sub('\,\,+', ' , , , ', new_word)
+            temp_dict[word] = new_word
+            
+    temp_dict = {k: v for k, v in temp_dict.items() if k != v}
+    data = list(map(lambda x: ' '.join([_make_dict_cleaning(i,temp_dict) for i in x.split()]), data))
+    return data
 
