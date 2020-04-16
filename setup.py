2044cbb708d25dded924bdfcb7057c14ac7e4b82
Ankur3107 <ankur310794@gmail.com>
2020-04-16 18:42:29 +0530

    new version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='nlp_preprocessing',  
-     version='0.1.3',
+     version='0.1.4',
      author="Ankur Singh",
      author_email="ankur310794@gmail.com",
      description="A Package for text preprocessing",
