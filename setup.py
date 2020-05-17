f8d3291ce8445d7f5eaf6ab0322fbfc4b790bf38
Ankur3107 <ankur310794@gmail.com>
2020-05-17 11:45:16 +0530

    bug fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='nlp_preprocessing',  
-     version='0.1.5',
+     version='0.1.6',
      author="Ankur Singh",
      author_email="ankur310794@gmail.com",
      description="A Package for text preprocessing",
